74b75b363313b0ec7e73bc98e962440c57c12c23
Tickly <807081817@qq.com>
2021-12-29 23:20:39 +0800

    打包

--- dist/content.js
+++ dist/content.js
@@ -1,1 +1,1 @@
-console.log("Hello World");
+import{$ as e}from"./assets/vendor.62917bef.js";window.addEventListener("load",()=>{const[o]=/(\d)+/.exec(location.pathname),t=e("#tpc .j_u").attr("uid");console.log(o,t)});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('Hello World')
+import $ from 'jquery'
+
+window.addEventListener('load', () => {
+  // 帖子id
+  const [tid] = /(\d)+/.exec(location.pathname) as Array<string>;
+  // 作者id
+  const uid = $('#tpc .j_u').attr('uid');
+
+
+  console.log(tid, uid)
+})
