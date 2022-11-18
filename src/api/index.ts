d72f5c855da6c12a74a37a6ca73519d9f2fce90d
taoguo <807081817@qq.com>
2022-11-18 17:32:01 +0800

    记录一下接口

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 const host = 'https://bbs.mobileapi.hupu.com';
 const path = '/3/7.3.8/threads/';
 
+// https://games.mobileapi.hupu.com/3/8.0.26/bplapi/user/v1/page
+// bddid=BTUFPSTZ4PO43PXRMOG4ZFYU6N6OLXAHVT5SZVDDHN3QCGS3DGDQ01&client=d1b270c0342f561134b55d234c27d0eef6ca4530&clientId=39736037&componentCode=skin_app_mine_page&crt=1668739856&deviceId=Buefzyf5jT9N3oEJiM0%2B5bzgPDEDUpCeZEz%2BfAjYk7GxI6twxdDm3GE8y/onS5OYC1EWH4adpPeJwC%2B1tgNxLcA%3D%3D&newapp=1&night=0&puid=73218817&sign=1d17f298e554791c63e25198a9bb06c6&time_zone=Asia/Shanghai
+
 export interface Reply {
   pid: string;
   check_reply_info?: {
--- src/demo/App.vue
+++ src/demo/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
+    <div class="reply-list-avatar">
+      <a href="https://my.hupu.com/48984328187712" target="_blank" class="reply-list-avatar-wrapper">
+        <div class="lazyload-wrapper">
+          <img class="avatar" src="https://i2.hoopchina.com.cn/user/712/48984328187712/48984328187712-1652185199.png@150h_150w_2e" alt="替补首席库里" />
+        </div>
+      </a>
+    </div>
     <button class="btn btn-primary" @click="onClick">查看回复</button>
 
-    <reply-view
-      v-for="r in DialogProps"
-      :key="r.pid"
-      :tid="r.tid"
-      :pid="r.pid"
-    />
+    <reply-view v-for="r in DialogProps" :key="r.pid" :tid="r.tid" :pid="r.pid" />
   </div>
 </template>
 <script lang="ts" setup>
@@ -18,3 +20,15 @@ const onClick = () => {
   open('54807408', '149566');
 };
 </script>
+<style lang="less">
+.reply-list-avatar {
+  margin-bottom: 1em;
+  img {
+    display: block;
+    width: 64px;
+    height: 64px;
+    border: 1px solid black;
+    overflow: hidden;
+  }
+}
+</style>
